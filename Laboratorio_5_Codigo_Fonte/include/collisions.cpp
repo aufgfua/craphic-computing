93465dbf9d1efed71d13311bf19537a5f52407c2
Augusto Flach <augustofalcaoflach@gmail.com>
2022-10-07 23:38:05 -0300

    tried sphere/face collision

--- Laboratorio_5_Codigo_Fonte/include/collisions.cpp
+++ Laboratorio_5_Codigo_Fonte/include/collisions.cpp
@@ -22,6 +22,7 @@ glm::vec3 getBoundingBoxMax(const char* object_name){
     return g_VirtualScene[object_name].bbox_max;
 }
 
+// sphere sphere collision
 bool ghostCollide(glm::vec4 pacman_position, OBJETO ghost){
     glm::vec3 bbox_min = getBoundingBoxMin("ghost");
     glm::vec3 bbox_max  = getBoundingBoxMax("ghost");
@@ -33,9 +34,107 @@ bool ghostCollide(glm::vec4 pacman_position, OBJETO ghost){
     return false;
 }
 
+float planeDistance(glm::vec4 point1, glm::vec4 point2, glm::vec4 point3, glm::vec4 pacman_position){
+    glm::vec4 plane_normal = crossproduct(point2 - point1, point3 - point1);
+    float distance = glm::dot(plane_normal, pacman_position - point1);
+    return distance;
+}
+
+float pointsDistance(glm::vec4 point1, glm::vec4 point2){
+    float distance = glm::length(point1 - point2);
+    return distance;
+}
+
+glm::vec4 normalizeVector(glm::vec4 vector){
+    float length = glm::length(vector);
+    glm::vec4 normalized_vector = vector / length;
+    return normalized_vector;
+}
+
+bool testFaceCollision(glm::vec4 point1, glm::vec4 point2, glm::vec4 point3, glm::vec4 point4, glm::vec4 pacman_position, float distance){
+    float distance1 = pointsDistance(point1, pacman_position);
+    float distance2 = pointsDistance(point2, pacman_position);
+    float distance3 = pointsDistance(point3, pacman_position);
+    float distance4 = pointsDistance(point4, pacman_position);
+
+    glm::vec4 point1_2_2 = point1 + (point2 - point1) * 0.5f;
+    glm::vec4 point2_3_2 = point2 + (point3 - point2) * 0.5f;
+    glm::vec4 point3_4_2 = point3 + (point4 - point3) * 0.5f;
+    glm::vec4 point4_1_2 = point4 + (point1 - point4) * 0.5f;
+
+    glm::vec4 midPoint = (point1_2_2 + point2_3_2 + point3_4_2 + point4_1_2) * 0.25f;
+
+    float distanceMid = pointsDistance(midPoint, pacman_position);
+
+    bool collidesWithCorners = distance1 < distance || distance2 < distance || distance3 < distance || distance4 < distance;
+
+    float distanceMid_1 = glm::dot(normalizeVector(point1_2_2), midPoint - pacman_position);
+    float distanceMid_2 = glm::dot(normalizeVector(point2_3_2), midPoint - pacman_position);
+    float distanceMid_3 = glm::dot(normalizeVector(point3_4_2), midPoint - pacman_position);
+    float distanceMid_4 = glm::dot(normalizeVector(point4_1_2), midPoint - pacman_position);
+
+    bool insideBox = distanceMid_1 < glm::length(point1_2_2) && distanceMid_2 < glm::length(point2_3_2) && distanceMid_3 < glm::length(point3_4_2) && distanceMid_4 < glm::length(point4_1_2);
+
+    float pacmanPlaneDistance = planeDistance(point1, point2, point3, pacman_position);
+
+    bool collidesWithPlane = pacmanPlaneDistance < distance && pacmanPlaneDistance > -distance;
+
+    return collidesWithCorners || (insideBox && collidesWithPlane);
+
+}
+
+
+
+
+
+// colisão Sphere com Square
 bool wallCollide(glm::vec4 pacman_position, OBJETO wall){
-    glm::vec3 bbox_min = getBoundingBoxMin("wall");
-    glm::vec3 bbox_max = getBoundingBoxMax("wall");
+    return false;
+    // Não funcionou :/
+    // Mas acho que pode ter sido quase
+
+
+    glm::vec3 bbox_min_base = getBoundingBoxMin("wall");
+    glm::vec4 bbox_min = glm::vec4(bbox_min_base.x, bbox_min_base.y, bbox_min_base.z, 1);
+
+    glm::vec3 bbox_max_base = getBoundingBoxMax("wall");
+    glm::vec4 bbox_max = glm::vec4(bbox_max_base.x, bbox_max_base.y, bbox_max_base.z, 1);
+
+
+    glm::mat4 model =
+                 Matrix_Translate(wall.trans.x, wall.trans.y, wall.trans.z)
+                * Matrix_Rotate_X(wall.rotat.x) * Matrix_Rotate_Y(wall.rotat.y) * Matrix_Rotate_Z(wall.rotat.z)
+                * Matrix_Scale(wall.scal.x, wall.scal.y, wall.scal.z);
+
+    bbox_min = model * bbox_min;
+    bbox_max = model * bbox_max;
+
+
+
+    glm::vec4 point1 = glm::vec4(bbox_min.x, bbox_min.y, bbox_min.z, 1);
+    glm::vec4 point2 = glm::vec4(bbox_max.x, bbox_min.y, bbox_min.z, 1);
+    glm::vec4 point3 = glm::vec4(bbox_min.x, bbox_max.y, bbox_min.z, 1);
+    glm::vec4 point4 = glm::vec4(bbox_min.x, bbox_min.y, bbox_max.z, 1);
+
+    glm::vec4 point5 = glm::vec4(bbox_max.x, bbox_max.y, bbox_min.z, 1);
+    glm::vec4 point6 = glm::vec4(bbox_max.x, bbox_min.y, bbox_max.z, 1);
+    glm::vec4 point7 = glm::vec4(bbox_min.x, bbox_max.y, bbox_max.z, 1);
+    glm::vec4 point8 = glm::vec4(bbox_max.x, bbox_max.y, bbox_max.z, 1);
+
+    float pacmanSize = 0.5f;
+    bool collidesWithFace1 = testFaceCollision(point1, point2, point5, point3, pacman_position, pacmanSize);
+    bool collidesWithFace2 = testFaceCollision(point1, point4, point6, point2, pacman_position, pacmanSize);
+    bool collidesWithFace3 = testFaceCollision(point1, point3, point7, point4, pacman_position, pacmanSize);
+    bool collidesWithFace4 = testFaceCollision(point8, point6, point4, point7, pacman_position, pacmanSize);
+    bool collidesWithFace5 = testFaceCollision(point8, point5, point2, point6, pacman_position, pacmanSize);
+    bool collidesWithFace6 = testFaceCollision(point8, point7, point3, point5, pacman_position, pacmanSize);
+
+    return collidesWithFace1 || collidesWithFace2 || collidesWithFace3 || collidesWithFace4 || collidesWithFace5 || collidesWithFace6;
+
+
+
+
+
     return false;
 }
 
